3cf3fad69f2325dacf7a99a5bf9c9097e11cc35d
LJG070 <dlwlsrn94@naver.com>
2021-02-05 23:16:29 +0900

    feature-member Member 엔티티 초기 생성자와 setter 생성

--- src/main/java/com/kookmin/team1/module/member/domain/Member.java
+++ src/main/java/com/kookmin/team1/module/member/domain/Member.java
@@ -1,10 +1,14 @@
 package com.kookmin.team1.module.member.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
 
 @Entity(name = "MEMBER")
+@NoArgsConstructor
 @Getter
 public class Member {
     @Id @GeneratedValue
@@ -35,4 +39,36 @@ public class Member {
     @Enumerated(EnumType.STRING)
     @Column(name="ROLE")
     private MemberRole role;
+
+    @Builder
+    public Member(String email, String password, String nickname, String name,
+                  String phoneNumber, String status, String role) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.status = MemberStatus.valueOf(status);
+        this.role = MemberRole.valueOf(role);
+    }
+
+    public void encodePassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public void changePassword(String password) {
+        this.password = password;
+    }
+
+    public void editNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void changeName(String name) {
+        this.name = name;
+    }
+
+    public void editPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
