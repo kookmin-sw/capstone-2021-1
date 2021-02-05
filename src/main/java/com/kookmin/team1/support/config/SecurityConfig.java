05a06119f9a43475db654d6876680ce1088d168f
LJG070 <dlwlsrn94@naver.com>
2021-02-05 23:37:36 +0900

    feature-member Spring security 초기 세팅 작업

---
+++ src/main/java/com/kookmin/team1/support/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.kookmin.team1.support.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().disable();
+
+        //restful 이므로 필요없음, 인증,인가는 jwt 방식으로 대체할 예정
+        http.csrf().disable();
+
+        //마찬가지로 restful이므로 stateless하게 감
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //초기 세팅
+        http.authorizeRequests()
+                .anyRequest().permitAll();
+
+        //TODO::AWS에 배포하기전에 CORS 세팅 추가해놔야함
+    }
+}
