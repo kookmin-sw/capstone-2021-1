61881c310eb063a2ee2df51cbdaacf2f524b8c65
LJG070 <dlwlsrn94@naver.com>
2021-02-05 23:28:22 +0900

    feature-member 이후 작업할 Oauth2 연동을 위한 RestTemplate와 Security의 password encoding을 위한 PasswordEncoder 빈 등록

--- src/main/java/com/kookmin/team1/Team1Application.java
+++ src/main/java/com/kookmin/team1/Team1Application.java
@@ -2,6 +2,10 @@ package com.kookmin.team1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Team1Application {
@@ -10,4 +14,13 @@ public class Team1Application {
         SpringApplication.run(Team1Application.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
