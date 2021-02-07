138ab56e3edf85ec3d5f5e4ee9c4d556460154fa
LJG070 <dlwlsrn94@naver.com>
2021-02-07 12:02:05 +0900

    feature-member JWT 인증 방식을 위한 시큐리티 필터 JwtAuthenticationFilter 추가

---
+++ src/main/java/com/kookmin/pm/web/MemberController.java
@@ -0,0 +1,9 @@
+package com.kookmin.pm.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+}
