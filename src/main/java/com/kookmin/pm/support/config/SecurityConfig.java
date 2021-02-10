9038bea5935d206515e7366b4a496e4380bbdaee
LJG070 <dlwlsrn94@naver.com>
2021-02-10 21:03:13 +0900

    feature-web 초기 테스트용 CORS 세팅과 권한 확인 세팅

--- src/main/java/com/kookmin/pm/support/config/SecurityConfig.java
+++ src/main/java/com/kookmin/pm/support/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.kookmin.pm.support.config;
 
+import com.kookmin.pm.module.member.domain.MemberRole;
 import com.kookmin.pm.support.util.JwtTokenProvider;
 import com.kookmin.pm.web.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -19,21 +24,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
-
-        //jwt 인증 필터 추가
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-
-        //restful 이므로 필요없음, 인증,인가는 jwt 방식으로 대체할 예정
         http.csrf().disable();
-
-        //마찬가지로 restful이므로 stateless하게 감
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         //초기 세팅
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/*/signin", "/*/signin/**", "/*/signup", "/social/**").permitAll()
+                .anyRequest().hasRole(MemberRole.USER.toString());
 
-        //TODO::AWS에 배포하기전에 CORS 세팅 추가해놔야함
+        //TODO::초기 세팅, 허용할 origin, header, method 제한을 줘야
+        http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
