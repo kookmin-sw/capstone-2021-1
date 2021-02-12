6070b0a17a9623829516bdd08255b2b383c24cd2
LJG070 <dlwlsrn94@naver.com>
2021-02-12 15:15:31 +0900

    feature-league LeagueParticipants 엔티티 생성

--- src/main/java/com/kookmin/pm/module/league/domain/LeagueParticipants.java
+++ src/main/java/com/kookmin/pm/module/league/domain/LeagueParticipants.java
@@ -1,5 +1,6 @@
 package com.kookmin.pm.module.league.domain;
 
+import com.kookmin.pm.module.member.domain.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,4 +14,16 @@ public class LeagueParticipants {
     @Id @GeneratedValue
     @Column(name = "LEAGUE_PARTICIPANTS_ID")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="LEAGUE_ID")
+    private League league;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="STATUS")
+    private LeagueParticipantsStatus status;
 }
