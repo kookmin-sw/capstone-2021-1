7352683b4d38ec413a3877ab7c305330e039c2d9
LJG070 <dlwlsrn94@naver.com>
2021-02-13 16:37:52 +0900

    feature-web findMemberInLeague 오탈자 수정

--- src/main/java/com/kookmin/pm/web/LeagueController.java
+++ src/main/java/com/kookmin/pm/web/LeagueController.java
@@ -1,13 +1,14 @@
 package com.kookmin.pm.web;
 
+import com.kookmin.pm.module.league.dto.LeagueCreateInfo;
 import com.kookmin.pm.module.league.service.LeagueLookupType;
 import com.kookmin.pm.module.league.service.LeagueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,5 +20,20 @@ public class LeagueController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(leagueService.lookupLeague(leagueId, LeagueLookupType.WITH_PARTICIPANTS));
+    }
+
+    @PostMapping(value = "/league")
+    public ResponseEntity openLeague(Principal principal,
+                                     @RequestBody LeagueCreateInfo leagueCreateInfo) {
+        Long usn = getPrincipalKey(principal);
+        leagueService.openLeague(usn, leagueCreateInfo);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("대회를 생성했습니다.");
+    }
+
+    private Long getPrincipalKey(Principal principal) {
+        return Long.parseLong(principal.getName());
     }
 }
