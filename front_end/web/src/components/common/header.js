8b334e8034fa752847acf4b472dd34aa98c4bcce
음승호 <dmatmdgh49@gmail.com>
2021-03-23 16:50:08 +0900

    link for header and home

--- front_end/web/src/components/common/header.js
+++ front_end/web/src/components/common/header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {Link} from "react-router-dom"
+import "../../assets/css/Common/common.css"
 import "../../assets/css/Common/header.css";
 import Hamburger_icon from '../../assets/images/common/Hamburger_icon.png';
 
@@ -7,9 +9,9 @@ function Header() {
     <div className="header">
       <div className="header_header">
         <img src={Hamburger_icon} alt="" className="hamburger_bar"/>
-        <div className="no_right_border to_enroll">회원 가입</div>
-        <div className="to_login">로그인</div>
-        <div className="to_home">홈</div>
+        <Link to="/enroll" className="common_link"><div className="no_right_border to_enroll">회원 가입</div></Link>
+        <Link to="/login" className="common_link"><div className="to_login">로그인</div></Link>
+        <Link to="/" className="common_link"><div className="to_home">홈</div></Link>
       </div>
       <div className="header_logo">
       <h1 className="logo_text">Play Maker</h1>
