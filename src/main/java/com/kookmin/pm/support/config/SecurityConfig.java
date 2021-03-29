816aefa53c1955f3472024d9d7ea59d738ffb769
LJG070 <dlwlsrn94@naver.com>
2021-03-29 10:59:24 +0900

    develop-hot-fix CORS 세팅 수정

--- src/main/java/com/kookmin/pm/support/config/SecurityConfig.java
+++ src/main/java/com/kookmin/pm/support/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.kookmin.pm.support.config;
 
 import com.kookmin.pm.module.member.domain.MemberRole;
 import com.kookmin.pm.support.util.JwtTokenProvider;
+import com.kookmin.pm.web.filter.CustomResponseFilter;
 import com.kookmin.pm.web.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
@@ -22,30 +24,28 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomResponseFilter customResponseFilter;
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().disable();
+        http.addFilterBefore(customResponseFilter, SecurityContextPersistenceFilter.class);
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+        http.httpBasic().disable();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         //초기 세팅
         http.authorizeRequests()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/**/signin", "/**/signin/**", "/**/signup", "/social/**").permitAll()
                 .antMatchers("/**/all").permitAll()
-                .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/**/search").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/crew/detail/*").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/matching/detail/*").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/league/detail/*").permitAll()
                 .anyRequest().hasRole(MemberRole.USER.toString());
 
-
-
-        //TODO::초기 세팅, 허용할 origin, header, method 제한을 줘야
         http.cors().configurationSource(corsConfigurationSource());
     }
 
@@ -53,9 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("**");
+        configuration.addAllowedHeader("**");
+        configuration.addAllowedMethod("**");
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
