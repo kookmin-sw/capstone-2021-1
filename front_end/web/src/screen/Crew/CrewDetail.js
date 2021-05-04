de3ce72096ba438dee16e05c76aa84f80690f92e
leeminkyu97 <bomblmk97@naver.com>
2021-05-04 18:25:50 +0900

    crewDetail 초안

--- front_end/web/src/screen/Crew/CrewDetail.js
+++ front_end/web/src/screen/Crew/CrewDetail.js
@@ -9,20 +9,24 @@ class CrewDetail extends React.Component{
 
     render(){
         const {location} = this.props;
+        console.log(location);
         return (
-            <div className="crew_detail_container">
-                <div className="crew_detail_crew_info">
-                    <div className="crew_image">
-                        <div className="backbtn"></div>
+            <div className="crewDetail">
+                <div className="crewDetail_crewInfo">
+                    <div className="crewDetail_crewInfo_image">
+                        <div className="crewDetail_crewInfo_image_backBtn">뒤로가기버튼</div>
+                        <div className="crewDetail_crewInfo_image_image">이미지</div>
                     </div>
-                    <div className="crew_info">
-                        <div className="crew_title">
-                        </div>
-                        <div className="crew_detail_info">
-                        </div>
+                    <div className="crewDetail_crewInfo_info">
+                        <div className="crewDetail_crewInfo_info_title">크루이름</div>
+                        <div className="crewDetail_crewInfo_info_info">크루세부정보</div>
+                        <div className="crewDetail_crewInfo_info_intro">크루소개</div>
+                        <div className="crewDetail_crewInfo_info_joinBtn">가입하기</div>
                     </div>
                 </div>
-                <div className="crew_detail_crew_member">
+                <div className="crewDetail_crewMember">
+                    <div className="crewDetail_crewMember_title">모든 크루원</div>
+                    <div className="crewDetail_crewMember_list">크루원 리스트</div>
                 </div>
             </div>
             )
