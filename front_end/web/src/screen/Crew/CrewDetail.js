c0f516eff632d0cf5cd5c7b486d36f715b277cac
leeminkyu97 <bomblmk97@naver.com>
2021-05-09 23:26:55 +0900

    crewDetail 이미지랑 버튼 겹치기 제외하고 끝

--- front_end/web/src/screen/Crew/CrewDetail.js
+++ front_end/web/src/screen/Crew/CrewDetail.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../assets/css/Crew/CrewDetail.css";
+import BACK_ICON from "../../assets/images/common/backBtn_lmk.png"
+import PLUS_ICON from "../../assets/images/common/plus_lmk.png"
 
 class CrewDetail extends React.Component{
 
@@ -16,18 +18,21 @@ class CrewDetail extends React.Component{
             <div className="crewDetail">
                 <div className="crewDetail_crewInfo">
                     <div className="crewDetail_crewInfo_image">
-                        <div className="crewDetail_crewInfo_image_backBtn">뒤로가기버튼</div>
-                        <div className="crewDetail_crewInfo_image_image">이미지</div>
+                        <div className="crewDetail_crewInfo_image_backBtn">
+                            <img src={BACK_ICON}/>
+                        </div>
+                        <p>이미지</p>
                     </div>
                     <div className="crewDetail_crewInfo_info">
                         <div className="crewDetail_crewInfo_info_block1">
                             <div className="crewDetail_crewInfo_info_title">{location.state.data.name}</div>
-                            <div className="crewDetail_crewInfo_info_plusBtn">더하기</div>
+                            <div className="crewDetail_crewInfo_info_plusBtn">
+                                <img src={PLUS_ICON}/>
+                            </div>
                             <div className="crewDetail_crewInfo_info_adminBtn">크루관리</div>
                         </div>
                         <div className="crewDetail_crewInfo_info_block2">
-                            <div className="crewDetail_crewInfo_info_activityArea">지역: {location.state.data.activityArea}</div>
-                            <div className="crewDetail_crewInfo_info_participantsCount">총모집인원: {location.state.data.participantsCount}</div>
+                            <div className="crewDetail_crewInfo_info_activityArea">지역: {location.state.data.activityArea} 총모집인원: {location.state.data.participantsCount}</div>
                             <div className="crewDetail_crewInfo_info_category">카테고리: {location.state.data.category}</div>
                         </div>
                         <div className="crewDetail_crewInfo_info_description">{location.state.data.description}</div>
@@ -36,7 +41,18 @@ class CrewDetail extends React.Component{
                 </div>
                 <div className="crewDetail_crewMember">
                     <div className="crewDetail_crewMember_title">모든 크루원</div>
-                    <div className="crewDetail_crewMember_list">크루원 리스트</div>
+                    <div className="crewDetail_crewMember_list">
+                        <div className="crewDetail_crewMember_list_image">
+                            <p>얼굴</p>
+                        </div>
+                        <div className="crewDetail_crewMember_list_name">
+                            <p>크루원 이름</p>
+                        </div>
+                        <div className="crewDetail_crewMember_list_intro">
+                            <p>크루원 소개</p>
+                        </div>
+                        <hr size="1" color="#bcbcbc"></hr>
+                    </div>
                 </div>
             </div>
             )
