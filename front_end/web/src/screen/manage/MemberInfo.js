cad422588d3d6c81330206314aa803f25bc9a31e
leeminkyu97 <bomblmk97@naver.com>
2021-05-14 18:39:17 +0900

    크루페이지(상세) 뼈대, 전역변수 아직 안넣음

--- front_end/web/src/screen/manage/MemberInfo.js
+++ front_end/web/src/screen/manage/MemberInfo.js
@@ -1,4 +1,7 @@
 import React from "react";
+import "../../assets/css/Manage/MemberInfo.css";
+import BACK_ICON from "../../assets/images/common/backBtn_lmk.png"
+import PLUS_ICON from "../../assets/images/common/plus_lmk.png"
 
 class MemberInfo extends React.Component{
     
@@ -7,7 +10,43 @@ class MemberInfo extends React.Component{
         const MEMBER_DATA = this.props.location.data;
         // 전역변수 MEMBER_DATA에 필요한 데이터가 전부 들어있습니다.
         return (
-            <div className=""></div>
+            <div className="memberInfo">
+                <div className="memberInfo_block1">
+                    <div className="memberInfo_block1_BackBtn">
+                        <img src={BACK_ICON}/>
+                    </div>
+                    <div className="memberInfo_block1_PlusBtn">
+                        <img src={PLUS_ICON}/>
+                    </div>
+                    <div className="memberInfo_block1_AdminBtn">
+                        크루관리
+                    </div>
+                </div>
+                <div className="memberInfo_block2">
+                    <div className="memberInfo_block2_info">
+                        <div className="memberInfo_block2_info_image">
+                            <p>얼굴</p>
+                        </div>
+                        <div className="memberInfo_block2_info_name">
+                            <p>이름</p>
+                        </div>
+                        <div className="memberInfo_block2_info_ageAndGender">
+                            <p>나이/성별</p>
+                        </div>
+                    </div>
+                    <div className="memberInfo_block2_description">
+                        <p>소개글</p>
+                    </div>
+                </div>
+                <div className="memberInfo_block3">
+                    <div className="memberInfo_block3_detail">
+                        상세정보
+                    </div>
+                    <div className="memberInfo_block3_graph"></div>
+                    <div className="memberInfo_block3_release">방출하기</div>
+                    
+                </div>
+            </div>
             )
     }
 }
